9fa6e620722795843c37f4feb4b18517ffe0ef4d
kprodromou <kprodromou@IITL0056.athens.intrasoft-intl.private>
2019-06-14 10:25:07 +0300

    Add GetLastName module

---
+++ GetLastNameModule/src/main/java/com/Intra/getlastnamemodule/GetLastNameClass.java
@@ -0,0 +1,18 @@
+package com.Intra.getlastnamemodule;
+
+import java.util.Scanner;
+
+public class GetLastNameClass {
+
+	public static String getLastName() {
+		
+		
+		System.out.println("Please enter your last name:");
+		Scanner in = new Scanner(System.in);
+		String lastName = in.nextLine();
+		return lastName;
+		
+	}
+	
+	
+}
